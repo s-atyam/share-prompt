9cbde797dd8a952ec51e38d0b171a99a1af95c94
Satyam <satyambharti080@gmail.com>
2023-12-28 18:41:40 +0530

    version 1

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import '@styles/globals.css'
+
+export const metadata = {
+  title:'Propmtopia',
+  description:'Discover & share AI prompts'
+}
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+      <body>
+        <Provider>
+          <div className='main'>
+            <div className='gradient'/>
+          </div>
+          <main className='app'>
+            <Nav/>
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
