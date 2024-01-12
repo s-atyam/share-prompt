70695612dfe06576ebb6e69fa96c22a1d67663f0
Satyam <satyambharti080@gmail.com>
2024-01-12 11:12:26 +0530

    small fixes

--- app/page.jsx
+++ app/page.jsx
@@ -6,10 +6,10 @@ const Home = () => {
         <h1 className="head_text text-center">
             Discover and Share
             <br className="min-md:hidden"/>
-            <span className="blue_gradient text-center">AI-powered prompts</span>
+            <span className="blue_gradient text-center">AI Prompts</span>
         </h1>
         <p className="desc text-center">
-            Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+            Discover Muse is an open-source prompting tool for modern world to discover, create and share creative AI prompts
         </p>
         <Feed/>
     </section>
